b6603ae598209397c1725ec8ab2acae54e92a222
fengsansei <1707795704@qq.COM>
2024-07-17 00:16:12 +0800

    e，this is shit

---
+++ 2024-7-16/2024-7-16/源.c
@@ -0,0 +1,34 @@
+#define   _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int t = 0;
+	scanf("%d%d%d", &a, &b, &c);
+	 if (a < b)
+	 {
+		 t = a;
+		 a = b;
+		 b = t;
+
+	    }
+	 if (a < c)
+	 {
+		 t = a;
+		 a = c;
+		 c = t;
+
+	 } 
+	 if (b < c)
+	 {
+		 t = b;
+		 b = c;
+		 c = t;
+
+	 }
+	 printf("%d %d %d", a, b, c);
+	 return 0;
+
+}
